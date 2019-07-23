af302779e3df9546a10354fb8bd25e50bf25dd62
Guilherme da Silva <guilherme.dasilvanascimento@gmail.com>
2019-07-23 00:01:25 -0300

    CRUD na entity Loja

---
+++ CRUDLojas/Data/CRUDLojasContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDLojas.Models
+{
+    public class CRUDLojasContext : DbContext
+    {
+        public CRUDLojasContext (DbContextOptions<CRUDLojasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDLojas.Models.Loja> Loja { get; set; }
+    }
+}
---
+++ CRUDLojas/Models/Loja.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDLojas.Models
+{
+    public class Loja
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string DataCadastro { get; set; }
+    }
+}
