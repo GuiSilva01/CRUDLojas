4aa961fd58cacc75c62e8d5557ab0a2f4de28ca3
Guilherme da Silva <guilherme.dasilvanascimento@gmail.com>
2019-07-23 22:57:38 -0300

    Adicionando link cadastro de lojas na nave bar e traduzindo

--- CRUDLojas/Controllers/HomeController.cs
+++ CRUDLojas/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace CRUDLojas.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Sua página de contato.";
 
             return View();
         }
